a40a0ddaa46a9c323b88f10e30baccd3188698c9
Léo Levacher <leo.levacher@actency.fr>
2025-01-25 12:50:20 +0100

    refactor(searchWord): rework the code to make it cleaner

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import { Index } from "../types/types";
+import { Index } from '../types/types';
 
-const isPerfectSquare = (num: number): boolean => {
+export const isPerfectSquare = (num: number): boolean => {
   const sqrt = Math.sqrt(num);
   return sqrt === Math.floor(sqrt);
 };
@@ -27,7 +27,7 @@ export const getInitialIndexes = (words: string[], matrix: string[][]) => {
   for (let y = 0; y <= matrix.length - 1; y++) {
     for (let x = 0; x <= matrix[y].length - 1; x++) {
       const letter = matrix[y][x];
-      const foundLetter = initials.find((initial) => initial === letter)
+      const foundLetter = initials.find((initial) => initial === letter);
       if (foundLetter)
         results.push({
           letter: foundLetter,
@@ -39,100 +39,98 @@ export const getInitialIndexes = (words: string[], matrix: string[][]) => {
   return results;
 };
 
-const isCorrectWord = (word: string, matrix: string[][], func: (row: number, col: number) => Omit<Index, "letter">, startingIndex: Omit<Index, "letter">) => {
-  let currentIndex = startingIndex
+export const isCorrectWord = (
+  word: string,
+  matrix: string[][],
+  func: (row: number, col: number) => Omit<Index, 'letter'>,
+  startingIndex: Omit<Index, 'letter'>
+) => {
+  let currentIndex = startingIndex;
 
   for (let letterIndex = 0; letterIndex < word.length - 1; letterIndex++) {
     if (word[letterIndex] !== matrix[currentIndex.row][currentIndex.col])
-      return {isFound: false};
+      return { isFound: false };
     if (letterIndex !== word.length - 1)
       currentIndex = func(currentIndex.row, currentIndex.col);
   }
 
-  return {isFound: true, startingPos: startingIndex, endingPos: currentIndex};
-}
+  return { isFound: true, startingPos: startingIndex, endingPos: currentIndex };
+};
 
-export const searchWord = (word: string, matrix: string[][], startingIndex: Index) => {
-  const wordLength = word.length;
+export const searchWord = (
+  word: string,
+  matrix: string[][],
+  startingIndex: Index
+) => {
+  const directions = [
+    {
+      name: 'vertical reversed',
+      func: (row: number, col: number) => ({ row: row - 1, col }),
+      condition: () => startingIndex.row >= word.length - 1
+    },
+    {
+      name: 'diagonal to top right',
+      func: (row: number, col: number) => ({ row: row - 1, col: col + 1 }),
+      condition: () =>
+        startingIndex.row >= word.length - 1 &&
+        startingIndex.col + word.length - 1 <= matrix[0].length
+    },
+    {
+      name: 'horizontal',
+      func: (row: number, col: number) => ({ row, col: col + 1 }),
+      condition: () => startingIndex.col + word.length - 1 <= matrix[0].length
+    },
+    {
+      name: 'diagonal to bottom right',
+      func: (row: number, col: number) => ({ row: row + 1, col: col + 1 }),
+      condition: () =>
+        startingIndex.col + word.length - 1 <= matrix[0].length &&
+        startingIndex.row + word.length - 1 <= matrix.length
+    },
+    {
+      name: 'vertical',
+      func: (row: number, col: number) => ({ row: row + 1, col }),
+      condition: () => startingIndex.row + word.length - 1 <= matrix.length
+    },
+    {
+      name: 'diagonal reverse to bottom left',
+      func: (row: number, col: number) => ({ row: row + 1, col: col - 1 }),
+      condition: () =>
+        startingIndex.row + word.length - 1 <= matrix.length &&
+        startingIndex.col >= word.length - 1
+    },
+    {
+      name: 'horizontal reversed',
+      func: (row: number, col: number) => ({ row, col: col - 1 }),
+      condition: () => startingIndex.col >= word.length - 1
+    },
+    {
+      name: 'diagonal reverse to top left',
+      func: (row: number, col: number) => ({ row: row - 1, col: col - 1 }),
+      condition: () =>
+        startingIndex.row >= word.length - 1 &&
+        startingIndex.col >= word.length - 1
+    }
+  ];
 
-  const isFittingInHeight = startingIndex.row + wordLength - 1 <= matrix.length
-  const isFittingInLength = startingIndex.col + wordLength - 1 <= matrix[0].length
-  const isFittingInHeightRev = startingIndex.row >= wordLength - 1
-  const isFittingInLengthRev = startingIndex.col >= wordLength - 1
-
-  // console.log("Checking word:", word, "Starting index:", startingIndex)
-
-  if (isFittingInHeightRev) {
-    const toTheTop = (row: number, col: number) => ({row: row - 1, col})
-    const isCorrect = isCorrectWord(word, matrix, toTheTop, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: vertical reversed", startingIndex, isCorrect.endingPos)
-      return true
+  for (const direction of directions) {
+    if (direction.condition()) {
+      const isCorrect = isCorrectWord(
+        word,
+        matrix,
+        direction.func,
+        startingIndex
+      );
+      if (isCorrect.isFound) {
+        console.log(
+          `Word found! Direction: ${direction.name}`,
+          startingIndex,
+          isCorrect.endingPos
+        );
+        return true;
+      }
     }
   }
 
-  if (isFittingInHeightRev && isFittingInLength) {
-    const toTheTopRight = (row: number, col: number) => ({row: row - 1, col: col + 1})
-    const isCorrect = isCorrectWord(word, matrix, toTheTopRight, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: diagonal to top right", startingIndex, isCorrect.endingPos)
-      return true
-    }
-  }
-
-  if (isFittingInLength) {
-    const toTheRight = (row: number, col: number) => ({row, col: col + 1})
-    const isCorrect = isCorrectWord(word, matrix, toTheRight, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: horizontal", startingIndex, isCorrect.endingPos)
-      return true
-    }
-  }
-
-  if (isFittingInLength && isFittingInHeight) {
-    const toTheBottomRight = (row: number, col: number) => ({row: row + 1, col: col + 1})
-    const isCorrect = isCorrectWord(word, matrix, toTheBottomRight, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: diagonal to bottom right", startingIndex, isCorrect.endingPos)
-      return true
-    }
-  }
-
-  if (isFittingInHeight) {
-    const toTheBottom = (row: number, col: number) => ({row: row + 1, col})
-    const isCorrect = isCorrectWord(word, matrix, toTheBottom, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: vertical", startingIndex, isCorrect.endingPos)
-      return true
-    }
-  }
-
-  if (isFittingInHeight && isFittingInLengthRev) {
-    const toTheBottomLeft = (row: number, col: number) => ({row: row + 1, col: col - 1})
-    const isCorrect = isCorrectWord(word, matrix, toTheBottomLeft, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: diagonal reverse to bottom left", startingIndex, isCorrect.endingPos)
-      return true
-    }
-  }
-
-  if (isFittingInLengthRev) {
-    const toTheLeft = (row: number, col: number) => ({row, col: col - 1})
-    const isCorrect = isCorrectWord(word, matrix, toTheLeft, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: diagonal reverse to bottom left", startingIndex, isCorrect.endingPos)
-      return true
-    }
-  }
-
-  if (isFittingInHeightRev && isFittingInLengthRev) {
-    const toTheTopLeft = (row: number, col: number) => ({row: row - 1, col: col - 1})
-    const isCorrect = isCorrectWord(word, matrix, toTheTopLeft, startingIndex)
-    if (isCorrect.isFound) {
-      console.log("Word found! Direction: diagonal reverse to top left", startingIndex, isCorrect.endingPos)
-      return true
-    }
-  }
-  // console.log("No word found :(")
-  return false
-}
+  return false;
+};
